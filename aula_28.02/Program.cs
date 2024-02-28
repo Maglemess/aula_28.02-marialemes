7ff1acdaed3d547e78c5b7eac89ef97f200fa640
CJ3016862 <CJ3016862@LAB01-PC44>
2024-02-28 09:03:18 -0300

    Adicionar arquivos de projeto.

---
+++ aula_28.02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_28._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario;
+            //leitura do salario
+            Console.WriteLine("Digite o valor do salario");
+            salario = float.Parse (Console.ReadLine());
+
+            salario = salario + (25.0f /100.0f) + salario;
+
+            Console.WriteLine("o novo salario é R${0}", salario);
+            Console.ReadKey();
+        
+        }
+    }
+}
